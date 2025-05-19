332f95fd5473c2ec65f8ce98d04f15fa2695fe20
Aakash yadav <126560106+aakashy5990@users.noreply.github.com>
2025-05-19 09:02:46 +0000

    Done

--- Hoisting/Script.js
+++ Hoisting/Script.js
@@ -1,2 +1,28 @@
 // Hoising are read the code bottom to up and variable declaration is hoisting and variable initilization is ot a hoising 
-// in below showing the example of the hosing 
+// in below showing the example of the hosing
+
+// for example of hoisting :- 
+// x = 20;
+// console.log(x);
+// var x;
+
+// hoising function
+// callme();
+
+// function callme(){
+//   console.log("THis is the Hoisting function ");
+// }
+
+
+// in below showing the example of the unhosing  its is also throw the undefine error
+
+// let x;
+// console.log(x)
+// x = 20;
+
+// to avoid the hoisting we can use the use strict 
+
+"use strict";
+x = 20;
+console.log(x);
+var x;
